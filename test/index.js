178caafa136ee05bcdeb537ff36f1156b4bf4bdd
stupid cat <kirkbenm@gmail.com>
2017-03-07 14:50:33 -0700

    Better error handling

--- test/index.js
+++ test/index.js
@@ -1,15 +1,23 @@
 const brainfuck = new (require('../'))();
+const util = require('util');
 var times = 0;
 
 function test(code, input) {
     console.log(`Test ${++times} ------------------------------------------------------`);
     const start = Date.now();
     let val;
+    let error;
     try {
-        val = brainfuck(code, input);
-    } catch (err) { val = err.message; }
-    const end = Date.now();
-    console.log('Output:', val, '\nResponse time:', end - start, 'ms');
+        val = brainfuck.execute(code, input);
+    } catch (err) { 
+        val = err.result;
+        error = err.message;
+    }
+    console.log(`${error !== undefined ? '        Error : ' + error + '\n' : ''}       Output : ${val.output}
+       Memory : ${util.inspect(val.memory)}
+        Steps : ${val.steps}
+Response Time : ${Date.now() - start}
+`);
 }
 
 // Hello World - Basic
